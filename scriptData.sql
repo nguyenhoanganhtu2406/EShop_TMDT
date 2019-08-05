87880dba54ceead52c1afebab693dc6c58d76c95
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2019-08-05 13:58:17 +0700

    First init

---
+++ scriptData.sql
@@ -0,0 +1,65 @@
+﻿use ESHOP
+
+INSERT INTO HANGHOA 
+VALUES('DAM NU 0006R','ESAO006','damnu_red.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006B','ESAO006','damnu_blue.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006Y','ESAO006','damnu_yellow.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006W','ESAO006','damnu_white.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006B1','ESAO006','damnu_blue1.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006PK','ESAO006','damnu_pink.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006LB','ESAO006','damnu_lightblue.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006BL','ESAO006','damnu_black.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006FS','ESAO006','damnu_freestyle.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006FS1','ESAO006','damnu_freestyle1.jpg',300000,0.05,N'ĐẦM NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('DAM NU 0006VJ','ESAO006','chanvay_jean.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VCR','ESAO008','chanvay_cream.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VBL','ESAO008','chanvay_black.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VW','ESAO008','chanvay_white.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VCR','ESAO008','chanvay_caro.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VCR','ESAO008','chanvay_caro1.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VFS','ESAO008','chanvay_freestyle.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('CHAN VAY 0006VFS1','ESAO008','chanvay_freestyle1.jpg',300000,0.05,N'CHÂN VÁY NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A1','ESAO006','aokieu_1.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A1','ESAO010','aokieu_1.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A2','ESAO010','aokieu_2.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A3','ESAO010','aokieu_3.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A4','ESAO010','aokieu_4.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A5','ESAO010','aokieu_5.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A6','ESAO010','aokieu_6.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO KIEU 0006A7','ESAO010','aokieu_7.jpg',300000,0.05,N'ÁO KIỂU NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q1','ESAO012','quanshort_1.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q2','ESAO012','quanshort_2.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q3','ESAO012','quanshort_3.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q4','ESAO012','quanshort_4.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q5','ESAO012','quanshort_5.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('QUAN SHORT 0006Q6','ESAO012','quanshort_6.jpg',300000,0.05,N'QUẦN SHORT NỮ ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q1','ESAO009','aothun_1.jpg',300000,0.05,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q2','ESAO009','aothun_2.jpg',300000,0.05,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q3','ESAO009','aothun_3.jpg',300000,0.05,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q4','ESAO009','aothun_4.jpg',300000,0.05,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q5','ESAO009','aothun_5.jpg',300000,0.05,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q1','ESAO013','aothunbalo_1.jpg',200000,0.1,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q2','ESAO013','aothunbalo_2.jpg',200000,0.1,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q3','ESAO013','aothunbalo_3.jpg',200000,0.1,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q4','ESAO013','aothunbalo_4.jpg',200000,0.1,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null),
+('AO THUN 0006Q5','ESAO013','aothunbalo_5.jpg',200000,0.1,N'ÁO THUN NAM ĐẸP SANG TRỌNG','ESHOP',null)
+
+INSERT INTO TINTUC
+VALUES(N'Khuyến mãi Noel',N'SALE KỊCH SÀN MÙA GIÁNG SINH CUỐI NĂM ĐÂY CÁC NÀNG CỦA NHÀ ESHOP Ạ ❤️
+- GIẢM NGAY 50 NGHÌN ĐỒNG KHI MUA BẤT KỲ SẢN PHẢM NÀO CỦA BEMINE, 1 SẢN PHẨM CŨNG GIẢM VÀ KHÔNG GIỚI HẠN SỐ LƯỢNG NÈ, MUA CÀNG NHIỀU ƯU ĐÃI CÀNG CAO   
+  CHÀO THU ĐÔNG " XINH NGẤT NGÂY" với áo khoác, đầm , áo dài, set... đốn tim người nhìn của #BeMine
+Team không thể thiếu cho mùa thu đông , những màu sắc HOT đang lên ngôi, chất liệu mềm mượt, dày dặn.
+ Không kén người mặc , không kén ốm, béo, cao, thấp , trắng, đen nà.
+ Nhanh chân đến với #ESHOP kẻo hết hàng nhen.
+ Có thể kiểm trả hàng trước khi thanh toán
+
+  ESHOP','damnu_pink.jpg','2018-12-21',1,'TT_KM',0),
+  (N'Khuyến mãi tết tây',N'SALE KỊCH SÀN MÙA TẾT  ĐÂY CÁC NÀNG CỦA NHÀ ESHOP Ạ ❤️
+- GIẢM NGAY 50 NGHÌN ĐỒNG KHI MUA BẤT KỲ SẢN PHẢM NÀO CỦA BEMINE, 1 SẢN PHẨM CŨNG GIẢM VÀ KHÔNG GIỚI HẠN SỐ LƯỢNG NÈ, MUA CÀNG NHIỀU ƯU ĐÃI CÀNG CAO   
+  CHÀO THU ĐÔNG " XINH NGẤT NGÂY" với áo khoác, đầm , áo dài, set... đốn tim người nhìn của #BeMine
+Team không thể thiếu cho mùa thu đông , những màu sắc HOT đang lên ngôi, chất liệu mềm mượt, dày dặn.
+ Không kén người mặc , không kén ốm, béo, cao, thấp , trắng, đen nà.
+ Nhanh chân đến với #ESHOP kẻo hết hàng nhen.
+ Có thể kiểm trả hàng trước khi thanh toán
+
+  ESHOP','aokieu_1.jpg','2018-12-21',1,'TT_KM',0)
